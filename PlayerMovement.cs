8bd64e55460f80eeca801b61cc052e80e605984f
Alex Lu <alexhonglu@gmail.com>
2025-09-13 02:48:10 -0400

    Added rotation and updated movement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    public float turnSpeed = 20f;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the Animator component attached to the player
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
+
+        // Identify if there is any input
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        // Rotate the player to face the direction of movement
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void onAnimatorMove()
+    {
+        // Move the player to the position based on animation movement
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
