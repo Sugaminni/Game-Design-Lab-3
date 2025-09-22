859ef31ab23d34ce1648b6a5b8a33d06b4ba6f8a
Alex Lu <alexhonglu@gmail.com>
2025-09-21 22:28:25 -0400

    Commented GameEnding.cs

--- GameEnding.cs
+++ GameEnding.cs
@@ -5,17 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
+    // Variables for fading the image and audio
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup; 
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource caughtAudio;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
-    
-    void OnTriggerEnter (Collider other)
+    bool m_HasAudioPlayed;
+
+    // If the player enters the trigger, switches the atExit flag to true
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
@@ -23,38 +28,48 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-    public void CaughtPlayer ()
+    // If player is caught, switches the caught flag to true
+    public void CaughtPlayer()
     {
         m_IsPlayerCaught = true;
     }
 
-    void Update ()
+    // Shows the appropriate image and plays the appropriate audio based on whether the player reached the exit or was caught
+    void Update()
     {
-        if (m_IsPlayerAtExit)
+        if(m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup, false);
+            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
         }
-        else if (m_IsPlayerCaught)
+        else if(m_IsPlayerCaught)
         {
-            EndLevel (caughtBackgroundImageCanvasGroup, true);
+            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    // Handles the fading of the image and audio when the player either reaches the exit or is caught
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            if (doRestart)
+            if(doRestart)
             {
-                SceneManager.LoadScene (0);
+                SceneManager.LoadScene(0);
             }
             else
             {
-                Application.Quit ();
+                Application.Quit();
             }
         }
     }
 }
+
