b3f9c97184a469d1306bfc7c80cd528d62ed48f7
Alex Lu <alexhonglu@gmail.com>
2025-09-21 22:23:00 -0400

    Updated PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,45 +4,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Variables for movement and shooting
+    public float turnSpeed = 20f;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
-    public float turnSpeed = 20f;
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+    public GameObject bullet;     // bullet prefab 
+    public Transform shotSpawn;   // shot spawn point 
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        // Get the Animator component attached to the player
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
+    // Checks for shoot input in Update
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+    }
+
+    // Move the player in FixedUpdate
     void FixedUpdate()
     {
+        // Stores the input axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize();
 
-        // Identify if there is any input
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
 
-        // Rotate the player to face the direction of movement
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        // Plays footstep sound
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        // Creates a rotation based on the movement vector
+        Vector3 desiredForward = Vector3.RotateTowards(
+            transform.forward, 
+            m_Movement, 
+            turnSpeed * Time.deltaTime, 
+            0f
+        );
         m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 
+    // Apply animation to rigidbody
     void OnAnimatorMove()
     {
-        // Move the player to the position based on animation movement
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(
+            m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude
+        );
         m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    // shoots a bullet
+    void Shoot()
+    {
+        GameObject g = Instantiate(bullet, shotSpawn.position, shotSpawn.rotation) as GameObject;
+        Destroy(g, 1.5f);  // auto-destroy after 1.5s
     }
 }
