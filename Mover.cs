cba198c71f6699f5d21a4843a7525c6b7c23ee72
Alex Lu <alexhonglu@gmail.com>
2025-09-21 22:16:02 -0400

    Added Mover script for bullet

---
+++ Mover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Start() // called before the first frame update
+    {
+        float speed = 20;
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    void OnCollisionEnter(Collision collision) // called when this collider/rigidbody has begun touching another rigidbody/collider
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);   // destroys enemy
+            Destroy(gameObject);             // destroys bullet
+        }
+        else
+        {
+            Destroy(gameObject);             // destroys bullet on wall/floor/etc.
+        }
+    }
+}
